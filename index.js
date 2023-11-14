b40e150626e016defe91874240f16804870c36de
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-11-13 21:27:15 -0600

    added function promptUser

--- index.js
+++ index.js
@@ -30,3 +30,43 @@ function writeToFile(fileName, answers) {
         err ? console.log(err) : console.log("Generated logo.svg");
       });
     }
+
+    function promptUser() {
+        inquirer
+          .prompt([
+            {
+                type: "input",
+                message:
+                  "What text would you like you logo to display? (Enter up to three characters)",
+                name: "text",
+              },
+              {
+                type: "input",
+                message:
+                  "Choose text color (Enter color keyword OR a hexadecimal number)",
+                name: "textColor",
+              },
+              {
+                type: "list",
+                message: "What shape would you like the logo to render?",
+                choices: ["Triangle", "Square", "Circle"],
+                name: "shape",
+              },
+              {
+                type: "input",
+                message:
+                  "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+                name: "shapeBackgroundColor",
+              },
+            ])
+            .then((answers) => {
+                if (answers.text.length > 3) {
+                    console.log("Must enter a value of no more than 3 characters");
+                    promptUser();
+                  } else {
+                    writeToFile("logo.svg", answers);
+                }
+              });
+          }
+
+          promptUser();
