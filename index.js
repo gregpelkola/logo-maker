8d36f4b022b7d0f8a2da71cc021743453ddf4217
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-11-29 21:09:07 -0600

    debug, fixed issue with test failing

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const fs = require("fs");
 
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
+// function to save answers to file
 function writeToFile(fileName, answers) {
     let svgString = "";
     svgString =  '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svgString += "<g>";
     svgString += `${answers.shape}`;
 
+    // pick shape for logo
     let shapeChoice;
     if (answers.shape === "Triangle") {
         shapeChoice = new Triangle();
@@ -31,6 +33,7 @@ function writeToFile(fileName, answers) {
       });
     }
 
+    // prompts user for a response
     function promptUser() {
         inquirer
         .prompt([
